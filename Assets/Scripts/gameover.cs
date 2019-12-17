9d0e7cb529bb69fbce4c3b0da5af7fa9556ddf59
Marshwells <nicolas.villaman@virginiogomez.cl>
2019-12-17 19:30:41 -0300

    Initial Commit

---
+++ Assets/Scripts/gameover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void menu() {
+        Application.LoadLevel(0); 
+    }
+    
+}
